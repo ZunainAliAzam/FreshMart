8186926e1d2362c0340391a08a5ac1af326ea33f
ZunainAliAzam <zunainazam456@gmail.com>
2023-08-05 22:50:15 +0500

    commit changes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let searchBar=document.querySelector('#search');
-document.querySelector("#menu-btn").onclick=() =>{
+let searchBar = document.querySelector(".searchbar");
+let searchBtn = document.querySelector("#search-btn");
+
+searchBtn.onclick = () => {
     searchBar.classList.toggle('active');
 }
